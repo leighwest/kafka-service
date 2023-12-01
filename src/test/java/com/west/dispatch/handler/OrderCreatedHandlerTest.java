39af4bee89d40c10a8363ee4417ca2ffd67dfcdb
Leigh West <leighwest@hotmail.com>
2023-12-01 19:57:06 +0000

    Initial commit

---
+++ src/test/java/com/west/dispatch/handler/OrderCreatedHandlerTest.java
@@ -0,0 +1,26 @@
+package com.west.dispatch.handler;
+
+import com.west.dispatch.service.DispatchService;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
+class OrderCreatedHandlerTest {
+
+    private OrderCreatedHandler handler;
+    private DispatchService dispatchServiceMock;
+
+    @BeforeEach
+    void setUp() {
+        dispatchServiceMock = mock(DispatchService.class);
+        handler = new OrderCreatedHandler(dispatchServiceMock);
+    }
+
+    @Test
+    void listen() {
+        handler.listen("payload");
+        verify(dispatchServiceMock, times(1)).process("payload");
+    }
+}
